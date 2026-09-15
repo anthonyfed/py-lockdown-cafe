22e5f3855342fe0183913d36e854d85bed08bdbf
anthonyfed <117385043+anthonyfed@users.noreply.github.com>
2026-09-15 12:21:31 -0400

    Solution #2 for lockdown cafe

--- app/cafe.py
+++ app/cafe.py
@@ -17,7 +17,7 @@ class Cafe:
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("Visitor's vaccine is outdated.")
 
-        if not visitor["wearing_a_mask"]:
+        if not visitor.get("wearing_a_mask"):
             raise NotWearingMaskError("Visitor is not wearing a mask.")
 
         return f"Welcome to {self.name}"
